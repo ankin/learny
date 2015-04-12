ecd7f51e826ad7e2476eaedf81b499a14dad62a6
ankin <akinash@gmail.com>
2015-04-12 23:00:16 +0200

    mongo references resolution unsing template helper

--- learny.js
+++ learny.js
@@ -1,14 +1,14 @@
   GermanWords = new Mongo.Collection("germanWords"
     //, {
     //transform: function(doc) {
-      //doc.enWords.forEach(function(englishWord) {
-        
-        //doc.enWordsRef = doc.enWordsRef || [];
-        //var enWord = EnglishWords.findOne(englishWord);
-       // doc.enWordsRef.push(EnglishWords.findOne(englishWord));
-      //});
+    //doc.enWords.forEach(function(englishWord) {
+
+    //doc.enWordsRef = doc.enWordsRef || [];
+    //var enWord = EnglishWords.findOne(englishWord);
+    // doc.enWordsRef.push(EnglishWords.findOne(englishWord));
+    //});
     //}
-  //}
+    //}
   )
 
   EnglishWords = new Mongo.Collection("englishWords");
@@ -18,9 +18,23 @@
   if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
-      words: function() {
-        return GermanWords.find({});
+        words: function() {
+          return GermanWords.find({});
+        }
+
       }
+
+    );
+
+    Template.englishWordsTpl.helpers({
+      englishWords: function() {
+        var englishWords = [];
+        this.enWords.forEach(function(enWord){
+          englishWords.push(EnglishWords.findOne(enWord));
+        });
+        return englishWords;
+      }
+
     });
 
 
